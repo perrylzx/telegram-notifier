9225137d2a0ba1839b53013e069727dba5ddd13e
perrylzx <perrylzx@gmail.com>
2025-08-04 16:55:11 +0800

    Use okhttp library and enqueue network requests

--- src/main/java/com/telegramnotifier/TelegramNotifierService.java
+++ src/main/java/com/telegramnotifier/TelegramNotifierService.java
@@ -1,17 +1,21 @@
 package com.telegramnotifier;
 
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
 
 import javax.inject.Inject;
 import java.io.IOException;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 
 public class TelegramNotifierService
 {
     private final TelegramNotifierConfig config;
+
+    @Inject
+    private OkHttpClient okHttpClient;
 
     @Inject
     public TelegramNotifierService(TelegramNotifierConfig config)
@@ -21,18 +25,37 @@ public class TelegramNotifierService
 
     public void sendMessage(String message)
     {
-        try {
-            String TELEGRAM_BOT_TOKEN = config.telegramBotToken();
-            String CHAT_ID = config.telegramChatId();
-            String urlStr = String.format(
-                    "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s",
-                    TELEGRAM_BOT_TOKEN, CHAT_ID, URLEncoder.encode(message, StandardCharsets.UTF_8)
-            );
-            HttpURLConnection conn = (HttpURLConnection) new URL(urlStr).openConnection();
-            conn.setRequestMethod("GET");
-            conn.getInputStream().close();
-        } catch (IOException e) {
-            log.warn("Error while sending telegram message: {}", e.getMessage());
-        }
+        String TELEGRAM_BOT_TOKEN = config.telegramBotToken();
+        String CHAT_ID = config.telegramChatId();
+
+        String urlStr = String.format(
+                "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s",
+                TELEGRAM_BOT_TOKEN,
+                CHAT_ID,
+                URLEncoder.encode(message, StandardCharsets.UTF_8)
+        );
+
+        Request request = new Request.Builder()
+                .url(urlStr)
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback()
+        {
+            @Override
+            public void onFailure(Call call, IOException e)
+            {
+                log.debug("Failed to send Telegram message: ", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response)
+            {
+                try (response) {
+                    if (!response.isSuccessful()) {
+                        log.debug("Failed to send Telegram message: {}", response.code());
+                    }
+                }
+            }
+        });
     }
 }
